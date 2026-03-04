68d3646a4a3681868f4b7e237e1e1703f18e6a0d
Mekala Sanjana <sanjanamekala916@gmail.com>
2026-03-04 20:03:56 +0530

    Add files via upload

---
+++ Day-22/packages/main.py
@@ -0,0 +1,12 @@
+'''from cal.add import add
+from cal.sub import sub
+from cal.mul import mul
+from cal.div import div'''
+from cal import add,sub,mul,div
+
+
+print(add(4,5))
+print(sub(4,5))
+print(mul(4,5))
+print(div(4,5))
+
